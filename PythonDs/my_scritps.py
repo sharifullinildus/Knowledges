ef329e2d75ea9ee55370fd8d81ac06313012e41e
Ildus Sharifullin SKHMS E <S2400289@hynixad.com>
2020-04-02 17:31:53 +0300

    heh

--- PythonDs/my_scritps.py
+++ PythonDs/my_scritps.py
@@ -1,5 +1,6 @@
 from sklearn import metrics as mt, model_selection as ms, preprocessing as pr
-from sklearn import tree
+from sklearn import tree as tr, neighbors as kn
+from sklearn
 import jupyter
 import sys
 
@@ -16,9 +17,53 @@ def kvad_kor(data, colums, add=0):
             data[i + '_kvad'] = data['kvad'].apply(lambda x: x ** 0.5)
     return data
 
+
 def choose_scaler(x,y,estimator):
     asd = 123
+
 
 def estimators_results(x,y,estimators, params, scaler, scorer=mt.make_scorer(mt.f1_score)):
     for estim in estimators:
         scaler.fit_transform(x,y)
+
+
+def dummies(data, features=()):
+    return pd.get_dummies(data.drop(features, axis=1)).copy()
+
+
+def get_estim_name(estimator):
+    return ''.join(re.findall('[A-Z]',estimator.__str__()[:estimator.__str__().index('(')]))
+
+
+def estimators_compare(x, y, estiamtors, params, scaler=pr.StandardScaler, scorer=mt.make_scorer(mt.f1_score), cv=10):
+    sc = scaler()
+    results = []
+    xsc = pd.DataFrame(sc.fit_transform(x))
+    for num, estiamtor in enumerate(estiamtors):
+        gr = ms.GridSearchCV(estiamtor, param_grid=params[num], scoring=scorer, n_jobs=-1, cv=cv)
+        gr.fit(xsc, y)
+        results.append((gr.best_score_, get_estim_name(estiamtor), gr.best_params_))
+    results = results.sort()
+    return results
+
+
+class Grid:
+    def __init__(self):
+        self.fitted = False
+
+    def fit(self, x, y):
+        self.fitted = True
+        self.grid.fit(x, y)
+        self.predictor = self.grid.best_estimator_
+
+    def predict(self,x):
+        return self.predictor.predict(x)
+
+
+class KNN(Grid):
+    def __init__(self, n_neighbors=(1,2,3,5,7,9,12,15),weights=('uniform','distance'),metric=('euclidean','manhattan'),
+                 cv=10, scorer=mt.make_scorer(mt.f1_score)):
+        Grid.__init__(self)
+        self.params = {'n_neighbors':n_neighbors, 'weights':weights,'metric':metric}
+        self.estim = kn.KNeighborsClassifier()
+        self.grid = ms.GridSearchCV(self.estim, param_grid=self.params, cv=cv, scoring=scorer, n_jobs=-1)
