bfe40e5a41589e9c15f2ad48123efa860f56dc74
Ildus Sharifullin SKHMS E <S2400289@hynixad.com>
2020-04-06 16:56:05 +0300

    trash add

---
+++ PythonDs/trash.py
@@ -0,0 +1,71 @@
+import random
+buk = ('q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m')
+
+
+
+
+def hash1(str):
+    has = 0
+    p=1
+    p2=1
+    p3=1
+    z=1
+    for i in str:
+        y=(ord(i))#+100000)%300
+        if has >184467440737095516:
+            has%=184467440737095516
+        has +=(y+p2)*(y+p3)*p+z*(y+p2)*(y+p3+p)+p*(y+p3*p2)*z
+        z+=1
+        p+=z
+        p2+=y%61
+        p3+=y%31
+    return has
+
+def unics():
+    words = []
+    for i in product(buk, repeat=4):
+        words.append(''.join(i))
+    for i in product(buk, repeat=3):
+        words.append(''.join(i))
+    for i in product(buk, repeat=2):
+        words.append(''.join(i))
+    for i in product(buk, repeat=1):
+        words.append(''.join(i))
+    z=0
+    asd =set()
+    for i in words:
+        z+=1
+        asd.add(hash1(i)%1000000)
+    print len(asd),len(words)
+
+def perestanovki():
+    def RandomPermutation():
+        perm = list(range(8))
+        random.shuffle(perm)
+        return perm
+
+    def StupidPermutation():
+        partialSums = [0, 1, 8, 35, 111, 285,
+                       628, 1230, 2191, 3606, 5546, 8039, 11056, 14506, 18242,
+                       22078, 25814, 29264, 32281, 34774, 36714, 38129, 39090,
+                       39692, 40035, 40209, 40285, 40312, 40319, 40320]
+        r = random.randint(0, partialSums[-1])
+        numInv = 0
+        while partialSums[numInv] < r:
+            numInv += 1
+        perm = list(range(8))
+        for step in range(numInv):
+            t1 = random.randint(0, 7)
+            t2 = random.randint(0, 7)
+            perm[t1], perm[t2] = perm[t2], perm[t1]
+        return perm
+
+    asd =0
+    zxc = 0
+    for i in range(10000):
+        asd += StupidPermutation()[7]
+        zxc += RandomPermutation()[7]
+
+    print asd, zxc
+
+unics()
