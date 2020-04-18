f6719a20277a0b992a1db8fec1667db3f5be363f
sharifullinildus <sharifullin.ildus@list.ru>
2020-04-18 09:37:32 +0300

    asd

--- PythonDs/stavki.py
+++ PythonDs/stavki.py
@@ -1,5 +1,5 @@
 #import pandas as pd
-import requests, re
+import requests, re, os
 
 def daily_football(date):
     try:
@@ -9,6 +9,7 @@ def daily_football(date):
         status = 0
     if status == 200:
         text = req.text
+        # Парсим день
         matches = re.findall(r'href="(/ru/soccer.*?)"',text)
         for match in matches:
             try:
@@ -18,10 +19,9 @@ def daily_football(date):
                 status = 0
             if status == 200:
                 text = req.text
+                # Парсим матч
+                head = re.findall('_25ClD f_12-16">(.*?)<',text)
 
 
-
-
-
-
-daily_football('2020-04-16')
+match_header = ('data','country','liga','team1','team2','winner','nichya','overtime','gols1','gols2','kart1',\
+                'jelt1','jelt2', )
